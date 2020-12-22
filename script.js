ba443cc0163ce2632f7068912286c0d10e0acb75
Michael Medina <michaelanthony.medinaa@gmail.com>
2020-12-21 21:15:41 -0800

    layout for highscores page created with button to link back to original game page

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 var timeEl = document.querySelector(".timer");
-
-
+var startEl = document.querySelector(".start-page");
+var questionsEl = document.querySelector(".questions");
+var endEl = document.querySelector(".end-game");
+var quizQuestions = [{
+    question: "Who played Jim's love interest that eventually became the regional manager of the Uitca Branch ?",
+    answers: { a: "Carol Stills",
+    b: "Pam Beesly",
+    c: "Karen Filippelli"},
+    answer: "c"
+},
+]
+ 
+// create varaibles to shorthand queryselector
 var secondsLeft = 90;
 
 function timer() {
@@ -15,4 +26,3 @@ function timer() {
     }, 1000);
 }
 
-timer();
